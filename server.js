bf5df11245f5839cb89919df11c157eb73d5819b
mthang1801 <mthang1801@gmail.com>
2020-04-01 16:05:30 +0700

    version 1.0.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/connectDB";
 import bodyParser from "body-parser";
 import items from "./routes/api/items";
+import path from "path";
 const app = express();
 //connect Database
 connectDB();
@@ -14,6 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //use routes
 app.use("/api/items", items);
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === "production"){
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
 
 const port = process.env.APP_PORT || 3000;
 const host = process.env.APP_HOST || "localhost";
