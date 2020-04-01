38d024f8fd06ed2e722e5fe19ab3a917cd75b230
mthang1801 <mthang1801@gmail.com>
2020-04-01 12:20:38 +0700

    v[1.0.0] connect to backend

--- client/src/actions/itemAction.js
+++ client/src/actions/itemAction.js
@@ -1,20 +1,42 @@
+import axios from "axios";
 import * as types from "./types";
 
-export const getItems = () => {
-  return{
-    type : types.GET_ITEMS
-  }
+export const getItems = () => dispatch =>  {
+  dispatch(setItemsLoading());
+  axios.get("/api/items")
+          .then(response => {
+            console.log(response);
+            dispatch({
+              type : types.GET_ITEMS,
+              payload : response.data
+            })
+          })
+          .catch(err => console.log(err));
+          
 }
 
-export const addItem = item => {
-  return {
-    type : types.ADD_ITEM,
-    payload : item 
-  }
+export const addItem = item => dispatch=>  {  
+  axios.post("/api/items", {name : item})
+          .then(response => {           
+            dispatch({              
+              type : types.ADD_ITEM,
+              payload : response.data               
+            })
+          })
 }
-export const deleteItem = id => {
-  return {
-    type : types.DELETE_ITEM,
-    payload : id 
+export const deleteItem = id => dispatch => {
+  axios.delete(`/api/items/${id}`)
+        .then(response => {
+          console.log(response);
+          dispatch({
+            type : types.DELETE_ITEM,
+            payload : id
+          })
+        }) 
+}
+
+export const  setItemsLoading = () => {
+  return{
+    type : types.ITEMS_LOADING
   }
 }
